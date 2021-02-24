592f73bec239707697d27db27b622ea223d6fd35
Aline Vignoli <nyhv.contato@gmail.com>
2021-02-24 15:18:10 -0300

    Criei o coordinator com as funções para navegar entre as rotas

---
+++ src/router/coordinator.js
@@ -0,0 +1,23 @@
+export const goToLogin = (history) => {
+    history.push('/login');
+};
+
+export const goToSignup = (history) => {
+    history.push('/signup');
+};
+
+export const goToCreateMusic = (history) => {
+    history.push('/music/create');
+};
+
+export const goToMusicFeed = (history) => {
+    history.push('/music');
+};
+
+export const goToMusicDetails = (history, musicId) => {
+    history.push(`/music/${musicId}`)
+};
+
+export const goBack = (history) => {
+    history.goBack();
+};
